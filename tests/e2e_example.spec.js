b5c572904edf08a30b5a8f37606b3a99cb47038a
wasim.jamadar <wasim.jamadar@veeasystems.com>
2023-08-16 18:28:59 +0100

    Adding e2e test and removed unwated test

--- tests/e2e_example.spec.js
+++ tests/e2e_example.spec.js
@@ -3,7 +3,7 @@ import {test, expect} from '@playwright/test';
 //############################################
 // Page Navigation & E2E test
 // 1. webui: test to login into account 
-// 2. api: get the available books to add.
+// 2. webui: delete all books for a user.
 // 3. webui: add a book from shelf.
 // 4. webui: goto profile and check if the book is added.
 // 5. api: Now use the api to get the books for the profile to verify if the book added via test is added.
@@ -33,8 +33,8 @@ test.describe('Testing adding book to profile', ()=>{
             type: 'task',
             description: 'Login and add book to user profile via webUI and check it via webui and api (backend info).',
         });
+
         // 1. webui: test to login into account 
-        
         await page.goto('https://demoqa.com/login');
         await page.getByPlaceholder('UserName').click();
         await page.getByPlaceholder('UserName').fill('test_username05');
@@ -42,34 +42,38 @@ test.describe('Testing adding book to profile', ()=>{
         await page.getByPlaceholder('Password').fill('Password01*');
         await page.getByRole('button', { name: 'Login' }).click();
 
-        // check for new url.
+        // check if navigated to new profile page after login.
         var url=await page.url();//get the url of the current page
         await expect(page).toHaveURL('https://demoqa.com/profile')
-        // use api to get all books for this user and print it.
-
-        // delete all books.
+        
+        
+        // 2. webui: delete all books for a user.
         await page.getByRole('button', { name: 'Delete All Books' }).click();
         await page.getByRole('button', { name: 'OK' }).click();
+        
 
-
+        // 3. webui: add a book from shelf.
         await page.locator('li').filter({ hasText: /^Book Store$/ }).click();
-        // use api to get all books for this user and print it.
         await page.getByRole('link', { name: 'Git Pocket Guide' }).click();
         await page.getByRole('button', { name: 'Add To Your Collection' }).click();
         await page.locator('li').filter({ hasText: 'Profile' }).click();
         await page.getByRole('gridcell', { name: 'Richard E. Silverman' }).click();
        
+
+        // 4. webui: goto profile and check if the book is added.
+
+        //const bookTitle = await page.getByRole('link', { name: 'Git Pocket Guide' }).getAttribute('id');
+        //const bookTitle = await page.getByRole('link', { name: 'Git Pocket Guide' }).getAttribute()
+        //console.log('book title  = ', response)
+        //await expect(bookTitle).toContain('see-book-Git Pocket Guide') 
         
+        
+        // 5. api: Now use the api to get the books for the profile to verify if the book added via test is added.
 
-        // 2. api: get the available books to add.
-
-
-        //await page.getByRole('button', { name: 'Log out' }).click();
-
-        //await page.close();
-        // get current page
-        // const {page}=this.helpers.Playwright;
-        // const url=await page.url();//get the url of the current page
-        // await expect(page).toHaveURL(someURL)
+       
+        // Logout and close the page.
+        await expect(page).toHaveURL('https://demoqa.com/profile')
+        await page.getByRole('button', { name: 'Log out' }).click();
+        await page.close();
   });
 });
