9d50f289043134d92d8b9ba7256673dd31763167
wasim.jamadar <wasim.jamadar@veeasystems.com>
2023-08-17 11:36:33 +0100

    completed e2e test commit

--- tests/e2e_example.spec.js
+++ tests/e2e_example.spec.js
@@ -1,4 +1,5 @@
 import {test, expect} from '@playwright/test';
+import { json } from 'stream/consumers';
 
 //############################################
 // Page Navigation & E2E test
@@ -26,14 +27,15 @@ test.afterEach(async ({ page }, testInfo) => {
         }
 })
 
-
-test.describe('Testing adding book to profile', ()=>{
-    test('test01', async ({ page }) => {
+test.describe.serial('Testing adding book to profile', ()=>{
+    test('test01_Add_and_verify_book_via_webui', async ({page}) => {
+        const USERNAME = "test_username05";
+        const PASSWORD = "Password01*";
         test.info().annotations.push({
             type: 'task',
-            description: 'Login and add book to user profile via webUI and check it via webui and api (backend info).',
+            description: 'Login, delete all books and add a book to user profile via webUI.',
         });
-
+        console.log('page == ', typeof page)
         // 1. webui: test to login into account 
         await page.goto('https://demoqa.com/login');
         await page.getByPlaceholder('UserName').click();
@@ -58,22 +60,76 @@ test.describe('Testing adding book to profile', ()=>{
         await page.getByRole('button', { name: 'Add To Your Collection' }).click();
         await page.locator('li').filter({ hasText: 'Profile' }).click();
         await page.getByRole('gridcell', { name: 'Richard E. Silverman' }).click();
-       
-
-        // 4. webui: goto profile and check if the book is added.
-
-        //const bookTitle = await page.getByRole('link', { name: 'Git Pocket Guide' }).getAttribute('id');
-        //const bookTitle = await page.getByRole('link', { name: 'Git Pocket Guide' }).getAttribute()
-        //console.log('book title  = ', response)
-        //await expect(bookTitle).toContain('see-book-Git Pocket Guide') 
         
+        
+        // 4. webui: goto profile page and check if the book is added.
+        await page.goto('https://demoqa.com/profile')
+        const bookVisible = await page.getByRole('link', { name: 'Git Pocket Guide' }).isVisible();
+        console.log("Book visible = ", bookVisible)
+        await expect(bookVisible == true).toBeTruthy()
         
         // 5. api: Now use the api to get the books for the profile to verify if the book added via test is added.
-
-       
+          
         // Logout and close the page.
-        await expect(page).toHaveURL('https://demoqa.com/profile')
+        await page.goto('https://demoqa.com/profile')
         await page.getByRole('button', { name: 'Log out' }).click();
         await page.close();
-  });
+    });
+
+    test('test02_Check_users_book_via_api', async ({ request }) => {
+        const USERNAME = "test_username05";
+        const PASSWORD = "Password01*";
+        test.info().annotations.push({
+            type: 'task',
+            description: 'User API to get user profile info and see available books matches the one added via webui',
+        });
+        // 1. Get Bearer token of the user.
+        const getToken = await request.post("https://demoqa.com/Account/v1/GenerateToken", {
+            data: {
+                "userName": USERNAME, 
+                "password": PASSWORD,
+            }   
+        })
+    
+        //console.log('\n token = ', await getToken.json())
+        let val = await getToken.json();    
+        let valtkn = val.token
+        console.log('\n -- > Token = ' + valtkn);
+
+        // 2. get userID of the user.
+        const getUser = await request.post("https://demoqa.com/Account/v1/Login", {
+            'headers': {
+                'authorization': 'Bearer ' + valtkn
+            },
+            data: {
+            "userName": USERNAME, 
+            "password": PASSWORD,
+        }   
+        })
+        
+        // console.log('\n userID = ', await getUser.json())
+        let valUserIdJson = await getUser.json();    
+        let userId = valUserIdJson.userId
+        console.log("\n -- > user id = " + userId);
+
+        const newUrl = "https://demoqa.com/Account/v1/User/" + userId
+        console.log('\n userdata url is '+newUrl)
+        
+        // 3. Get the book in the user profile.
+        let getUserData = await request.get( newUrl, {
+            'headers': {
+                'authorization': 'Bearer ' + valtkn
+                //'authorization': 'Bearer ' + tkn
+            },
+            data: {
+            "userName": USERNAME, 
+            "password": PASSWORD,
+        }   
+        })
+        // console.log('\n -- > userIdData = ', await getUserData.json())
+        let userDataJson = await getUserData.json();    
+        let userDatabooks = userDataJson.books
+        let Title = userDatabooks[0].title
+        console.log('Title of book = ', Title)
+    })
 });
